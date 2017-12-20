3c244606cb3e4ac431c3bc73b30a6d23f2955497
renkaiming <645236815@qq.com>
2017-12-20 22:16:37 +0800

    update

--- app/Libs/JSSDK.php
+++ app/Libs/JSSDK.php
@@ -235,7 +235,7 @@ class JSSDK {
         $urlObj["appid"] = env('WX_APPID','wx5dd4f5ae95592d61');//WxPayConfig::APPID;
         $urlObj["redirect_uri"] = "$redirectUrl";
         $urlObj["response_type"] = "code";
-        $urlObj["scope"] = "snsapi_userinfo";
+        $urlObj["scope"] = "snsapi_base";
         $urlObj["state"] = "STATE"."#wechat_redirect";
         $bizString = $this->ToUrlParams($urlObj);
         //$str = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf0e81c3bee622d60&redirect_uri=http%3A%2F%2Fnba.bluewebgame.com%2Foauth_response.php&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
