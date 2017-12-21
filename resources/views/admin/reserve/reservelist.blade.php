514530681f9f053282e41aa9c8c98a9b595d403b
renkaiming <645236815@qq.com>
2017-12-21 23:03:32 +0800

    update

--- resources/views/admin/reserve/reservelist.blade.php
+++ resources/views/admin/reserve/reservelist.blade.php
@@ -59,14 +59,34 @@
             {{ $list->render() }}
         </div>
     </div>
-    <div id="showMessage" style="display:none;">showMessage</div>
+    <div id="newplant" style="display:none">
+        <div class="winareabox">
+            <form class="form-horizontal" id = "ajaxform" role="form" method="post"  action="javascript:;">
+                <div class="form-group">
+                    <label class="col-sm-4 control-label" for="proid">商家名称：</label>
+                    <div class="col-sm-7">
+                        <input class="form-control" type="text" id="shopname"  name="shopname" placeholder="请输入商家名称">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-4 control-label" for="proid">商家url：</label>
+                    <div class="col-sm-7">
+                        <input class="form-control" type="text" id="shopurl"  name="shopurl" placeholder="请输入商家url">
+                    </div>
+                </div>
+                <div class="layui-layer-btn layui-layer-btn-c" >
+                    <button class="layui-btn layui-layer-btn0" type="submit">提交</button>
+                </div>
+            </form>
+        </div>
+    </div>
     <script>
         $("#btn").click(function() {
             layui.use('layer', function () {
                 var layer = layui.layer;
                 layer.open({
                     type: 1
-                    , title: '开通商户平台'
+                    , title: '编辑回访记录'
                     , area: '450px;'
                     , content: $('#showMessage')
                     , btnAlign: 'c' //按钮居中
