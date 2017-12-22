47946ca311d63de1f294f224483900444c5311d7
renkaiming <645236815@qq.com>
2017-12-22 12:18:38 +0800

    update

--- resources/views/admin/user/pushmsgdetail.blade.php
+++ resources/views/admin/user/pushmsgdetail.blade.php
@@ -58,7 +58,7 @@
                 return false;
             } else {
                 $.ajax({
-                    url: '/push/del',
+                    url: '/adm/push/del',
                     type: 'post',
                     dateType: 'json',
                     data: {id: id},
