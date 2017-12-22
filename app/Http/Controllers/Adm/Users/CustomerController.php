d43c07ca17c97894ea60245821a40ad433edc798
renkaiming <645236815@qq.com>
2017-12-22 16:18:29 +0800

    update

--- app/Http/Controllers/Adm/Users/CustomerController.php
+++ app/Http/Controllers/Adm/Users/CustomerController.php
@@ -69,8 +69,7 @@ class CustomerController extends Controller
     public function addPush(Request $request)
     {
         $phone = (int)$request->phone;
-        echo 123;
-        var_dump($request);exit;
+        echo 123;exit;
         if (empty($phone)) {
             return fun_error_view(0, '缺少参数', '/adm/push/pushmeslist');
         }
