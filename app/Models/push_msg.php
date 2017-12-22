5db30793e0874d844b276b738a8d2c3852404aa0
renkaiming <645236815@qq.com>
2017-12-22 12:00:07 +0800

    update

--- app/Models/push_msg.php
+++ app/Models/push_msg.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class push_msg extends Model
 {
     public $table = 'push_msg';
-    public $timestamps = false;
-    protected $fillable = ['id','phone','openid','ts'];
+    public $timestamps = true;
+    protected $fillable = ['id','phone','openid','created_at','updated_at'];
     public $primaryKey = "id";
 
 
