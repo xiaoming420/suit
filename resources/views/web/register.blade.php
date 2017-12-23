886432af502de7f903f92e02df17e14ecca0fa66
renkaiming <645236815@qq.com>
2017-12-23 13:12:21 +0800

    update 弹窗加载

--- resources/views/web/register.blade.php
+++ resources/views/web/register.blade.php
@@ -81,7 +81,9 @@
             ,btn: ['火速围观', '残忍拒绝']
             ,btnAlign: 'c'
             ,moveType: 1 //拖拽模式，0或者1
+            ,content: '<div style="padding: 50px; line-height: 22px; background-color: #393D49; color: #fff; font-weight: 300;">你知道吗？亲！<br>layer ≠ layui<br><br>layer只是作为Layui的一个弹层模块，由于其用户基数较大，所以常常会有人以为layui是layerui<br><br>layer虽然已被 Layui 收编为内置的弹层模块，但仍然会作为一个独立组件全力维护、升级。<br><br>我们此后的征途是星辰大海 ^_^</div>'
             , yes: function (index, layero) {
+
             }
         });
     @if(isset($info['phone']) && !empty($info['phone']))
