10302fee1017ebf71b49a3013e046d39aded9431
renkaiming <645236815@qq.com>
2017-12-23 16:51:49 +0800

    update

--- resources/views/web/register.blade.php
+++ resources/views/web/register.blade.php
@@ -100,7 +100,7 @@
     };*/
 
 
-    /*var Height=$(window).height();//取得浏览器页面可视区域的高度
+    var Height=$(window).height();//取得浏览器页面可视区域的高度
     var Width=$(window).width();
     layer.open({
         type: 1
@@ -118,15 +118,15 @@
 
     $(".cli").click(function() {
         layer.closeAll();
-    })*/
+    })
 
-    {{--@if(isset($info['phone']) && !empty($info['phone']))
+    @if(isset($info['phone']) && !empty($info['phone']))
         setTimeout(function () {
             alert('您已经注册过了！只能领取一次优惠哦！');
             WeixinJSBridge.call('closeWindow');
             // window.location.href = 'http://mp.weixin.qq.com/bizmall/mallshelf?id=&t=mall/list&biz=MzU3NTE0ODkyMg==&shelf_id=3&showwxpaytitle=1#wechat_redirect';
         },1000)
-    @endif--}}
+    @endif
 
 
 
