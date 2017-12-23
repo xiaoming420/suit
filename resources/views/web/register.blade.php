cd87bb4d374f7bdd23bab3a361a4fde952386f42
renkaiming <645236815@qq.com>
2017-12-23 15:56:07 +0800

    test

--- resources/views/web/register.blade.php
+++ resources/views/web/register.blade.php
@@ -110,7 +110,7 @@
         ,shade: 0.8
         ,id: 'LAY_layuipro' //设定一个id，防止重复弹出
         ,moveType: 1 //拖拽模式，0或者1
-        ,content: '<img style="width: 100%" src="/web/images/pp.png">'
+        ,content: '<img style="width: 100%;height: 100%" src="/web/images/pp.png">'
         , yes: function (index, layero) {
 
         }
