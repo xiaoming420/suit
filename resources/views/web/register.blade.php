aaf4bf4e5c816bbb9498ca73cdbafb2282cc5fd3
renkaiming <645236815@qq.com>
2017-12-23 15:50:03 +0800

    test

--- resources/views/web/register.blade.php
+++ resources/views/web/register.blade.php
@@ -101,11 +101,12 @@
 
 
     var Height=$(window).height();//取得浏览器页面可视区域的高度
+    var Width=$(window).width();
     layer.open({
         type: 1
         ,title: false //不显示标题栏
         ,closeBtn: false
-        ,area: Height+'px;'
+        ,area: [Height+'px;',Width+'px;']
         ,shade: 0.8
         ,id: 'LAY_layuipro' //设定一个id，防止重复弹出
         ,moveType: 1 //拖拽模式，0或者1
