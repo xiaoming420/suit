d64e21629a1b33b39f695db15bba1fb58a911eee
renkaiming <645236815@qq.com>
2017-12-23 13:50:03 +0800

    update 弹窗加载

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -24,7 +24,7 @@ class WebController extends Controller
     public function register()
     {
         $ali_or_wechat = fun_aliorwechat(); // 获取是在wechat打开还是ali打开
-        if ($ali_or_wechat != 1) return fun_error_page('请在微信客户端扫描打开');
+        if ($ali_or_wechat != 1) echo $_SERVER['HTTP_USER_AGENT'];//return fun_error_page('请在微信客户端扫描打开');
         if( !isset($_SESSION['open_id']) || empty($_SESSION['open_id']) ) {
             $tools = new JSSDK();
             $userInfo = $tools->__GetUserInfo();
